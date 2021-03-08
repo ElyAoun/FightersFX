27911b4cff2625d1b4c0cc72515bbd33bdaaa3d3
Elie Aoun <eaoun.2001@outlook.com>
2021-03-08 19:08:24 +0200

    FightersFX

---
+++ src/sample/MainMenuController.java
@@ -0,0 +1,66 @@
+package sample;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainMenuController {
+    static Stage stage;
+    static Scene scene;
+    @FXML
+    Button start_game, controls;
+
+    @FXML
+    void startGame() {
+        try {
+            final FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource(
+                            "FXMLfiles/ChooseCharacter.fxml"
+                    )
+            );
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1500, 750);
+            Music.musicPlayer.stop();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setFullScreen(true);
+            Music.generateSound("src/sample/Sounds/Menu/Choose Your Fighter.mp3");
+            ChooseCharacterController.scene = scene;
+            ChooseCharacterController.scene.getStylesheets().add(getClass().getResource("chooseCharacters.css").toExternalForm());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void controls(){
+        try {
+            final FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource(
+                            "FXMLfiles/Controls.fxml"
+                    )
+            );
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1500, 750);
+            Music.musicPlayer.stop();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setFullScreen(true);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void quit(){
+        stage.close();
+    }
+
+}
